3e040c62d156254a7ab122703068306d84fff14a
Tharunya <kmit.tharunya@gmail.com>
2015-07-22 19:11:16 +0530

    user functions

--- omod/src/main/java/org/openmrs/module/legacyui/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/legacyui/extension/html/AdminList.java
@@ -45,6 +45,7 @@ public class AdminList extends AdministrationSectionExt {
 	public Map<String, String> getLinks() {
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
 		map.put("/module/legacyui/manage.form", "legacyui.manage");
+		map.put("/module/legacyui/index.htm", "legacyui.adminui");
 		return map;
 	}
 	
