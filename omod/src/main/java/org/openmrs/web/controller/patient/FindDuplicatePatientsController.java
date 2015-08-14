cd2fb109d37b107cb999d06e9b4c4a17008e3881
Tharunya Pati <kmit.tharunya@gmail.com>
2015-08-14 18:49:14 +0530

    Update FindDuplicatePatientsController.java

--- omod/src/main/java/org/openmrs/web/controller/patient/FindDuplicatePatientsController.java
+++ omod/src/main/java/org/openmrs/web/controller/patient/FindDuplicatePatientsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FindDuplicatePatientsController {
 	@RequestMapping(value = "admin/patients/findDuplicatePatients")
-	public String displayAdminIndex(){
+	public String displayDuplicatePatients(){
 	return "module/legacyui/admin/patients/findDuplicatePatients";
 	}
 }
