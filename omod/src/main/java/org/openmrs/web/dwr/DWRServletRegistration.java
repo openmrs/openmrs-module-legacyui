18924832cef84f8d334a7c2967a85fedf6d2d9f7
dkayiwa <dkayiwa@openmrs.org>
2015-11-03 03:35:44 +0300

    Changing the base class of the dwr registration servlet

--- omod/src/main/java/org/openmrs/web/dwr/DWRServletRegistration.java
+++ omod/src/main/java/org/openmrs/web/dwr/DWRServletRegistration.java
@@ -13,15 +13,14 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-
-import org.directwebremoting.servlet.DwrServlet;
+import javax.servlet.http.HttpServlet;
 
 /**
  * This class is only here for the registration of the dwr servlet.
  * If we can get the ServletContext from any where in the module, we
  * can move this registration to there and get rid of this class.
  */
-public class DWRServletRegistration extends DwrServlet {
+public class DWRServletRegistration extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
