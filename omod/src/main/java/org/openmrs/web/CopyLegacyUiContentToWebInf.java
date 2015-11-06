f2a5275073588ac495f91838020d31b2505232da
wyclif <wyclif@openmrs.org>
2015-11-06 17:48:48 -0500

    Fixed CopyLegacyUiContentToWebInf which was copying some files to the strong directory, to avoid copying files multiple times and to avoid overwriting the core legacy index jsp

--- omod/src/main/java/org/openmrs/web/CopyLegacyUiContentToWebInf.java
+++ omod/src/main/java/org/openmrs/web/CopyLegacyUiContentToWebInf.java
@@ -11,6 +11,8 @@ package org.openmrs.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 
@@ -20,8 +22,10 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.ServletContextAware;
 
 public class CopyLegacyUiContentToWebInf implements ServletContextAware {
-
+	
 	private static Log log = LogFactory.getLog(CopyLegacyUiContentToWebInf.class);
+	
+	private static final String MODULE_ROOT_DIR = "/WEB-INF/view/module/legacyui";
 	
 	@Override
 	public void setServletContext(ServletContext servletContext) {
@@ -29,37 +33,57 @@ public class CopyLegacyUiContentToWebInf implements ServletContextAware {
 		String basePath = servletContext.getRealPath("");
 		
 		try {
-			//copy jsp
-			File destDir = new File(basePath + "/WEB-INF".replace("/", File.separator));
-			File srcDir = new File(basePath + "/WEB-INF/view/module/legacyui".replace("/", File.separator));
-			FileUtils.copyDirectory(srcDir, destDir);
-			
+			List<File> toIgnore = new ArrayList<>();
 			//copy error handler
 			File destFile = new File(basePath + "/errorhandler.jsp".replace("/", File.separator));
-			File srcFile = new File(basePath + "/WEB-INF/view/module/legacyui/errorhandler.jsp".replace("/", File.separator));
-			FileUtils.copyFile(srcFile, destFile);
+			File errorSrcFile = new File(basePath + MODULE_ROOT_DIR + "/errorhandler.jsp".replace("/", File.separator));
+			FileUtils.copyFile(errorSrcFile, destFile);
+			toIgnore.add(errorSrcFile);
+			
+			destFile = new File(basePath + "/index.jsp".replace("/", File.separator));
+			File indexSrcFile = new File(basePath + MODULE_ROOT_DIR + "/index.jsp".replace("/", File.separator));
+			FileUtils.copyFile(indexSrcFile, destFile);
+			toIgnore.add(indexSrcFile);
+			
+			destFile = new File(basePath + "/memoryUsage.jsp".replace("/", File.separator));
+			File memoryUsageSrcFile = new File(basePath + MODULE_ROOT_DIR + "/memoryUsage.jsp".replace("/", File.separator));
+			FileUtils.copyFile(memoryUsageSrcFile, destFile);
+			toIgnore.add(memoryUsageSrcFile);
+			
+			//Copy only the jsps under webapp to /WEB-INF/view
+			File destDir = new File(basePath + "/WEB-INF/view".replace("/", File.separator));
+			File srcDir = new File(basePath + MODULE_ROOT_DIR.replace("/", File.separator));
+			FileUtils.copyDirectory(srcDir, destDir,
+			    toCopy -> toCopy.getName().endsWith(".jsp") && !toIgnore.contains(toCopy));
 			
 			//copy scripts
 			destDir = new File(basePath + "/WEB-INF/view/scripts".replace("/", File.separator));
-			srcDir = new File(basePath + "/WEB-INF/view/module/legacyui/resources/scripts".replace("/", File.separator));
+			srcDir = new File(basePath + MODULE_ROOT_DIR + "/resources/scripts".replace("/", File.separator));
 			FileUtils.copyDirectory(srcDir, destDir);
 			
 			//copy openmrs.js
 			destFile = new File(basePath + "/openmrs.js".replace("/", File.separator));
-			srcFile = new File(basePath + "/WEB-INF/view/module/legacyui/resources/scripts/openmrs.js".replace("/", File.separator));
+			File srcFile = new File(basePath + MODULE_ROOT_DIR
+			        + "/resources/scripts/openmrs.js".replace("/", File.separator));
 			FileUtils.copyFile(srcFile, destFile);
 			
 			//copy images
 			destDir = new File(basePath + "/images".replace("/", File.separator));
-			srcDir = new File(basePath + "/WEB-INF/view/module/legacyui/resources/images".replace("/", File.separator));
+			srcDir = new File(basePath + MODULE_ROOT_DIR + "/resources/images".replace("/", File.separator));
 			FileUtils.copyDirectory(srcDir, destDir);
 			
 			//copy css
 			destDir = new File(basePath);
-			srcDir = new File(basePath + "/WEB-INF/view/module/legacyui/resources/css".replace("/", File.separator));
+			srcDir = new File(basePath + MODULE_ROOT_DIR + "/resources/css".replace("/", File.separator));
 			FileUtils.copyDirectory(srcDir, destDir);
+			
+			//copy the directories except for the files(JSPs) in the src
+			destDir = new File(basePath + "/WEB-INF".replace("/", File.separator));
+			srcDir = new File(basePath + MODULE_ROOT_DIR.replace("/", File.separator));
+			final File src = srcDir;
+			FileUtils.copyDirectory(src, destDir, toCopy -> toCopy.isDirectory() || !toCopy.getParentFile().equals(src));
 		}
-		catch(IOException ex) {
+		catch (IOException ex) {
 			log.error("Failed to copy legacy ui files", ex);
 		}
 	}
