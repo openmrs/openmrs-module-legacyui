308b4a5d609729bea9c58954507c8f7ff7f1dc01
teleivo <mrteleivo@gmail.com>
2016-09-02 14:39:57 +0200

    clean up license headers

--- omod/src/main/java/org/springframework/web/servlet/mvc/BaseCommandController.java
+++ omod/src/main/java/org/springframework/web/servlet/mvc/BaseCommandController.java
@@ -1,3 +1,12 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 /** NOTE: this class, which has been removed in Spring 4, has been copied over from Spring 3.2.7 to support old style SimpleFormControllers. */
 
 /*
