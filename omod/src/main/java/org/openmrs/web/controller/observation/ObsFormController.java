900b2efcfcb52e496f4f68643c6d1e61cfa43f98
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-01-01 23:05:06 +0100

    LUI-134 NullPointerException when voiding Observation

--- omod/src/main/java/org/openmrs/web/controller/observation/ObsFormController.java
+++ omod/src/main/java/org/openmrs/web/controller/observation/ObsFormController.java
@@ -192,8 +192,10 @@ public class ObsFormController extends SimpleFormController {
 				String view = getSuccessView() + "?encounterId=" + obs.getEncounter().getEncounterId() + "&phrase="
 				        + request.getParameter("phrase");
 				return new ModelAndView(new RedirectView(view));
-			} else {
+			} else if (newlySavedObs != null) {
 				return new ModelAndView(new RedirectView("obs.form?obsId=" + newlySavedObs.getObsId()));
+			} else {
+				return new ModelAndView(new RedirectView("obs.form?obsId=" + obs.getObsId()));
 			}
 		}
 		
