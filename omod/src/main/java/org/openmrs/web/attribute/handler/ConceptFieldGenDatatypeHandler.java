61691773b2316fbd4c7290ecc37f0cbabf585572
Michael Seaton <mseaton@pih.org>
2026-02-10 10:54:44 -0500

    LUI-207 - ConceptFieldGenDataTypeHandler should support handler configuration that allows customizing widget

--- omod/src/main/java/org/openmrs/web/attribute/handler/ConceptFieldGenDatatypeHandler.java
+++ omod/src/main/java/org/openmrs/web/attribute/handler/ConceptFieldGenDatatypeHandler.java
@@ -9,19 +9,25 @@
  */
 package org.openmrs.web.attribute.handler;
 
-import java.util.Map;
-
+import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.Concept;
+import org.openmrs.api.context.Context;
 import org.openmrs.customdatatype.CustomDatatype;
 import org.openmrs.customdatatype.datatype.ConceptDatatype;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handler for the Concept custom datatype
  */
 @Component
 public class ConceptFieldGenDatatypeHandler extends SerializingFieldGenDatatypeHandler<ConceptDatatype, Concept> {
-	
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	Map<String, Object> widgetConfiguration = new HashMap<>();
+
 	/**
 	 * @see SerializingFieldGenDatatypeHandler#getWidgetName()
 	 */
@@ -51,14 +57,33 @@ public class ConceptFieldGenDatatypeHandler extends SerializingFieldGenDatatypeH
 	 */
 	@Override
 	public Map<String, Object> getWidgetConfiguration() {
-		return null;
+		return widgetConfiguration;
 	}
 	
 	/**
 	 * @see SerializingFieldGenDatatypeHandler#setHandlerConfiguration(String)
 	 */
 	@Override
-	public void setHandlerConfiguration(String s) {
-		
+	public void setHandlerConfiguration(String handlerConfig) {
+		widgetConfiguration = new HashMap<>();
+		if (handlerConfig != null) {
+			try {
+				widgetConfiguration.putAll(objectMapper.readValue(handlerConfig, Map.class));
+			}
+			catch (Exception e) {
+				throw new IllegalArgumentException("Unable to parse widget configuration", e);
+			}
+			Object showAnswersRef = widgetConfiguration.remove("showAnswers");
+			if (showAnswersRef != null) {
+				// Concept concept = Context.getConceptService().getConceptByReference(showAnswersRef.toString());
+				Concept concept = Context.getConceptService().getConceptByUuid(showAnswersRef.toString());
+				if (concept != null) {
+					widgetConfiguration.put("showAnswers", concept.getConceptId());
+				}
+				else {
+					widgetConfiguration.put("showAnswers", Integer.parseInt(showAnswersRef.toString()));
+				}
+			}
+		}
 	}
 }
